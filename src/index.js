d6571c4d6d67ca9c19be9efe7eefd38477aa4339
Pratham Agrawal <prathamagrawal179@gmail.com>
2023-11-14 16:07:00 +0530

    curr date and time

--- src/index.js
+++ src/index.js
@@ -12,21 +12,27 @@
 
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import Heading from './Heading';
-import Para from './Para';
+// import Heading from './Heading';
+// import Para from './Para';
 
 const root = createRoot(document.getElementById('root'));
-
+const name = "Pratham";
+const date = new Date().toLocaleDateString();
+const time = new Date().toLocaleTimeString();
 root.render(
   <>
-    <Heading />
+    {/* <Heading />
     <Para />
     <ol>
       <li>pratham</li>
       <li>garv</li>
       <li>sai</li>
       <li>anubhav</li>
-    </ol>
+    </ol> */}
+  <h1>{`my name is ${name}`}</h1>
+  <p>{`Today's current date is ${date}`}</p>
+  <p>{`Today's current time is ${time}`}</p>
+    
   </>
 );
 
