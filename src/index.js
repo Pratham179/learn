ddd86d5343abf45a60ca09957d24524cecc297de
Pratham Agrawal <prathamagrawal179@gmail.com>
2023-11-21 17:55:48 +0530

    2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 // import ReactDom from 'react-dom';
 // import Heading from './Heading';
 // import Para from './Para';
-
+import './index.css';
 // ReactDom.render(
 //     <>
 //     <Heading/>
@@ -17,8 +17,8 @@ import { createRoot } from 'react-dom/client';
 
 const root = createRoot(document.getElementById('root'));
 const name = "Pratham";
-const date = new Date().toLocaleDateString();
-const time = new Date().toLocaleTimeString();
+// const date = new Date().toLocaleDateString();
+// const time = new Date().toLocaleTimeString();
 root.render(
   <>
     {/* <Heading />
@@ -29,9 +29,14 @@ root.render(
       <li>sai</li>
       <li>anubhav</li>
     </ol> */}
-  <h1>{`my name is ${name}`}</h1>
-  <p>{`Today's current date is ${date}`}</p>
-  <p>{`Today's current time is ${time}`}</p>
+  <h1 className='heading'>{`my name is ${name}`}</h1>
+  {/* <p>{`Today's current date is ${date}`}</p>
+  <p>{`Today's current time is ${time}`}</p> */}
+  <div className='img_div'>
+  <img className='img' src='https://picsum.photos/200/300'/>
+  <img className='img' src='https://picsum.photos/200/300'/>
+  <img className='img' src='https://picsum.photos/200/300'/>
+  </div>
     
   </>
 );
